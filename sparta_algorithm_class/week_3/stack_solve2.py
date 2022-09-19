750fa22f635290a8ba9a2f3d313c038feee34a14
hyeoKing <cksgurwkd12@naver.com>
2022-09-20 01:34:37 +0900

    feat: about stack, and just drill

--- sparta_algorithm_class/week_3/stack_solve2.py
+++ sparta_algorithm_class/week_3/stack_solve2.py
@@ -1,35 +1,37 @@
 def stack_sequence(n, sequence):
-
     result = []
     stack = []
     num = 1
     sequence_index = 0
 
-    while True:
+    while True: # stack을 1부터 오름 차순으로 증가시키면서, 현재 sequence_index의 값과 같은놈 나오면 stack.pop()
+        #TODO 1: 첫 시작시 stack에 1 추가
         if len(stack) == 0:
             stack.append(num)
-            num +=1
-            result.append("+")
+            num += 1
+            result.append('+')
+
+        #TODO 2: 현재 stack의 head와 sequence_index의 값이 같은 경우 pop
         elif sequence[sequence_index] == stack[-1]:
             stack.pop()
-            result.append("-")
+            result.append('-')
             sequence_index += 1
 
             if len(stack) == 0:
                 break
 
+        #TODO 3: 그렇지 않은 경우 그냥 num 증가, stack에 push
         else:
             if num > n:
+                print('no')
                 break
             stack.append(num)
-            result.append("+")
+            result.append('+')
             num += 1
 
     if len(stack) == 0:
         for target in result:
             print(target)
-
-
 
 sequence = list()
 
