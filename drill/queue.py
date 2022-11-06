60a51cb5849457befbb822f49e35ff476954bf9e
hyeoKing <cksgurwkd12@naver.com>
2022-11-06 16:21:48 +0900

    feat: stack, queue drill

--- drill/queue.py
+++ drill/queue.py
@@ -1,8 +1,7 @@
 
-
 class Node:
     def __init__(self, value):
-        self.data = value
+        self.value = value
         self.next = None
 
 
@@ -22,43 +21,103 @@ class Queue:
         self.tail.next = new_node
         self.tail = new_node
 
-        return
-
-    def peek(self):
-        if self.is_empty():
-            return "The queue is empty, cant use peek"
-        return self.head.data
-
     def dequeue(self):
         if self.is_empty():
-            return "The queue is empty, cant use dequeue"
-        delete_node = self.head
-        self.head = self.head.next
+            return "this queue is empty. you can't using dequeue()"
 
-        return delete_node.data
+        delete_data = self.head.value
+        self.head = self.head.next
+        return delete_data
 
     def is_empty(self):
         return self.head is None
 
+    def peek(self):
+        if self.is_empty():
+            return "this queue is empty. you can't using peek()"
+        return self.head.value
 
 
 
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+#
+# class Queue:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#
+#     def enqueue(self, value):
+#         new_node = Node(value)
+#
+#         if self.is_empty():
+#             self.head = new_node
+#             self.tail = new_node
+#             return
+#
+#         self.tail.next = new_node
+#         self.tail = new_node
+#         return
+#
+#     def dequeue(self):
+#         if self.is_empty():
+#             return "the queue is empty. you can't using dequeue()"
+#
+#         delete_data = self.head.value
+#         self.head = self.head.next
+#
+#         return delete_data
+#
+#
+#     def is_empty(self):
+#         return self.head is None
+#     def peek(self):
+#         if self.is_empty():
+#             return "the queue is empty. you can't using peek()"
+#         return self.head.value
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# class Node:
+#     def __init__(self, value):
+#         self.data = value
+#         self.next = None
+#
+#
+# class Queue:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#
+#     def enqueue(self, value):
+#         new_node = Node(value)
+#
+#         if self.is_empty():
+#             self.head = new_node
+#             self.tail = new_node
+#             return
+#
+#         self.tail.next = new_node
+#         self.tail = new_node
+#
+#         return
+#
+#     def peek(self):
+#         if self.is_empty():
+#             return "The queue is empty, cant use peek"
+#         return self.head.data
+#
+#     def dequeue(self):
+#         if self.is_empty():
+#             return "The queue is empty, cant use dequeue"
+#         delete_node = self.head
+#         self.head = self.head.next
+#
+#         return delete_node.data
+#
+#     def is_empty(self):
+#         return self.head is None
 
 
 # class Node:
--- drill/stack.py
+++ drill/stack.py
@@ -1,39 +1,127 @@
 
 
-
 class Node:
     def __init__(self, value):
-        self.data = value
         self.next = None
+        self.value = value
+
 
 class Stack:
     def __init__(self):
         self.head = None
 
-    #TODO 1: push
     def push(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
 
-    #TODO 2: pop
     def pop(self):
         if self.is_empty():
-            return "the Stack is Empty, can\'t apply pop"
-        delete_node = self.head
+            return "stack is empty. you can't using pop()."
+        delete_data = self.head.value
         self.head = self.head.next
 
-        return delete_node.data
+        return delete_data
 
-
-    #TODO 3: peek
-    def peek(self):
-        return self.head.data
-
-    #TODO 4: isEmpty
     def is_empty(self):
         return self.head is None
+    def peek(self):
+        return self.head.value
 
+
+
+# #TODO 1. Node를 만들어야 함. 이유는? 데이터 뿐 아니라 연관 노드의 주소도 저장하기 위해서
+#
+#
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+#
+#
+# class Stack:
+#     def __init__(self):
+#         self.head = None
+#
+#     def push(self, value):
+#         new_node = Node(value)
+#         new_node.next = self.head
+#         self.head = new_node
+#
+#     def pop(self):
+#         if self.is_empty():
+#             return "this stack is empty. you can't using pop()"
+#
+#         delete_data = self.head.value
+#         self.head = self.head.next
+#         return delete_data
+#     def is_empty(self):
+#         return self.head is None
+#
+#     def peek(self):
+#         return self.head.value
+
+# class Node:
+#     def __init__(self, value):
+#         self.data = value
+#         self.next = None
+#
+# class Stack:
+#     def __init__(self):
+#         self.head = None
+#     def push(self, value):
+#         new_node = Node(value)
+#         new_node.next = self.head
+#         self.head = new_node
+#
+#     def pop(self):
+#         #TODO 1. 스택의 맨 위 노드를 그냥 재껴주는거거덩요 ? (단, stack이 None 이 아닐때)
+#         if stack is None:
+#             return "스택이 비어있습니다. 비어있는 스택에 대해서 pop()할 수 없습니다."
+#
+#         pop_data = self.head.data
+#         self.head = self.head.next
+#         return f"pop success {pop_data}"
+#     def is_empty(self):
+#         return self.head is None
+#
+#     def peek(self):
+#         return self.head.data
+
+
+# class Node:
+#     def __init__(self, value):
+#         self.data = value
+#         self.next = None
+#
+# class Stack:
+#     def __init__(self):
+#         self.head = None
+#
+#     #TODO 1: push
+#     def push(self, value):
+#         new_node = Node(value)
+#         new_node.next = self.head
+#         self.head = new_node
+#
+#     #TODO 2: pop
+#     def pop(self):
+#         if self.is_empty():
+#             return "the Stack is Empty, can\'t apply pop"
+#         delete_node = self.head
+#         self.head = self.head.next
+#
+#         return delete_node.data
+#
+#
+#     #TODO 3: peek
+#     def peek(self):
+#         return self.head.data
+#
+#     #TODO 4: isEmpty
+#     def is_empty(self):
+#         return self.head is None
+#
 
 
 
