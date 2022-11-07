662b7c387f2e00abe363898162d397d79fe32a69
hyeoKing <cksgurwkd12@naver.com>
2022-11-07 22:37:19 +0900

    feat: small quiz

---
+++ sparta_algorithm_class/homework/ssg_discount.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+shop_prices = [30000, 2000, 1500000]
+user_coupons = [20, 40]
+
+
+def get_max_discounted_price(prices, coupons):
+    prices.sort()
+    coupons.sort()
+    result = 0
+
+    while prices and coupons:
+        origin = prices.pop()
+        result += int(origin * (100 - coupons.pop()) / 100)
+
+    if prices is not None:
+        while prices:
+            result += prices.pop()
+
+    return result
+
+
+print("정답 = 926000 / 현재 풀이 값 = ", get_max_discounted_price([30000, 2000, 1500000], [20, 40]))
+print("정답 = 485000 / 현재 풀이 값 = ", get_max_discounted_price([50000, 1500000], [10, 70, 30, 20]))
+print("정답 = 1550000 / 현재 풀이 값 = ", get_max_discounted_price([50000, 1500000], []))
+print("정답 = 1458000 / 현재 풀이 값 = ", get_max_discounted_price([20000, 100000, 1500000], [10, 10, 10]))
--- sparta_algorithm_class/homework/stack2.py
+++ sparta_algorithm_class/homework/stack2.py
@@ -1,22 +1,63 @@
+# def stack_sequence(n, sequence):
+#     count = 1
+#     stack = []
+#     result = []
+#
+#     for data in sequence:
+#         while count <= data:
+#             stack.append(count)
+#             count += 1
+#             result.append('+')
+#         if stack[-1] == data:
+#             stack.pop()
+#             result.append('-')
+#         else:
+#             print(stack)
+#             print("NO")
+#             exit(0)
+#     print('\n'.join(result))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def stack_sequence(n, sequence):
-    count = 1
+    pivot = 1
     stack = []
     result = []
 
-    for data in sequence:
-        while count <= data:
-            stack.append(count)
-            count += 1
-            result.append('+')
-        if stack[-1] == data:
-            stack.pop()
-            result.append('-')
-        else:
-            print(stack)
-            print("NO")
-            exit(0)
-    print('\n'.join(result))
+    while sequence:
+        target = sequence.pop()
 
+        # TODO 1. target까지 stack push
+        while pivot <= target:
+            stack.append(pivot)
+            result.append('+')
+            pivot += 1
+
+        # TODO 2. target 내용 pop()
+        if stack[-1] == target:
+            result.append('-')
+            stack.pop()
+
+        # TODO 3. 그 밖의 경우 announce 'NO', then exit()
+        else:
+            print("No")
+            exit(0)
+
+    print('\n'.join(result))
 
 
 sequence = list()
