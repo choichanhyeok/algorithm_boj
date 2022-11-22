1894b18cee9db831d39ee49ba1fa7a6e6b5ceccc
hyeoKing <cksgurwkd12@naver.com>
2022-11-23 00:48:28 +0900

    feat: heap_ex solved real

--- sparta_algorithm_class/homework/heap_ex.py
+++ sparta_algorithm_class/homework/heap_ex.py
@@ -7,15 +7,22 @@ supply_recover_k = 30
 
 
 def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
+    # (1) k일 이후에야 기존 공장이 정상 가동함
+    # (2) 해외 공장에서 지급받을 수 있는데 이를 최소화 하고 싶음
+    # (3) 해외 공장에서 밀가루 발주하는 날짜와 수량이 담긴 리스트를 이용해(dates, supplies)
+    # (4) 최소 몇번 해외 공장에 컨택 해야 하는지 찾아내기
+
+    # (5) stock(재고)가 k(재기동 날짜)와 같아질때까지 분석
+
     answer = 0
-    last_added_date_index = 0
     max_heap = []
+    date_index = 0
 
     while stock <= k:
-        while last_added_date_index < len(dates) and dates[last_added_date_index] <= stock:
-            heapq.heappush(max_heap, -supplies[last_added_date_index])
-            last_added_date_index += 1
-
+        while date_index < len(dates) and dates[date_index] <= stock: # 각 모든 입고 날짜에 stock이 정상적으로 동작할 수 있는지,
+                                                                      # 만약 그렇지 않다면 일단 아래 과정을 통해 stock 증가시키고 닷 ㅣ도전
+            heapq.heappush(max_heap, -supplies[date_index])
+            date_index += 1
         answer += 1
         heappop = heapq.heappop(max_heap)
         stock += -heappop
