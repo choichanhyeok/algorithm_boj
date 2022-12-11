206c3af8787ddce8f49253a205ba783ef3255ddf
hyeoKing <cksgurwkd12@naver.com>
2022-12-11 21:36:04 +0900

    feat: heap solved

--- sparta_algorithm_class/homework/ramen_factory.py
+++ sparta_algorithm_class/homework/ramen_factory.py
@@ -7,10 +7,20 @@ supply_recover_k = 30
 
 
 def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
+    max_heap = []
+    last_added_index = 0
+    answer = 0
 
+    while stock < k:
+        # 각 공급 날짜의 값들 중 최고 값 (heap을 이용)을 저장하다가, 공급 날짜까지 재고로 못 버티는 경우 stock를 공급 받아준다.
+        while last_added_index < len(dates) and stock >= dates[last_added_index]:
+            heapq.heappush(max_heap, -supplies[last_added_index])
 
+            last_added_index += 1
+        stock += -heapq.heappop(max_heap)
+        answer += 1
 
-    return
+    return answer
 
 
 print(get_minimum_count_of_overseas_supply(ramen_stock, supply_dates, supply_supplies, supply_recover_k))
