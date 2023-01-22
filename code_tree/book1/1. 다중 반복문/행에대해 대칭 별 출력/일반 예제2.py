f5994dc081fcc88e4f5bb260b5f221461fdfd9b9
hyeoKing <cksgurwkd12@naver.com>
2023-01-22 23:21:08 +0900

    feat: 행에 대해 대칭인 별 출력 (일반 예제2)

---
+++ code_tree/book1/1. 다중 반복문/행에대해 대칭 별 출력/일반 예제2.py
@@ -0,0 +1,31 @@
+# 정수 n의 값을 입력받아 별표를 출력하는 프로그램을 아래 예를 참고하여 작성해보세요.
+#
+# 예)
+#
+# n에 2를 입력받는 경우
+#
+# * *
+# *
+# * *
+# n에 3을 입력받는 경우
+#
+# * * *
+# * *
+# *
+# * *
+# * * *
+
+
+
+
+n = int(input())
+
+for i in range(n, 1, -1):
+    for _ in range(i):
+        print('*', end = ' ')
+    print()
+
+for i in range(1, n+1):
+    for _ in range(i):
+        print('*', end = ' ')
+    print()
